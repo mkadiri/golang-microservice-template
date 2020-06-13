b5ee410d22a62a8930034c8245985a2c42c19bc6
Mohammed Kadiri <m.kadiri@quidco.com>
2020-06-13 23:29:01 +0100

    more

--- {{cookie_cutter.repo_name}}/database/dev-fixtures/01-user.sql
+++
@@ -1,7 +0,0 @@
-truncate table `user`;
-
-INSERT INTO `user` (first_name, last_name, date_of_birth)
-VALUES
-    ('mohammed', 'kadiri', '1999-01-01'),
-    ('john', 'smith', '1992-02-02')
-;
--- {{cookie_cutter.repo_name}}/database/migrations/20180610205500_create_user_table.up.sql
+++
@@ -1,6 +0,0 @@
-create table `user` (
-    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-    `first_name` varchar(256) NOT NULL,
-    `last_name` varchar(256) NOT NULL,
-    `date_of_birth` datetime NOT NULL
-);
